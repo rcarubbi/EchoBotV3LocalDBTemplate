4a99e2320c239c43011d4993fac1912d289d92bb
Raphael Carubbi <rcarubbi@gmail.com>
2018-09-28 14:38:58 +0100

    Echo Bot Template V3 working with LocalDB

--- EchoBotV3/Global.asax.cs
+++ EchoBotV3/Global.asax.cs
@@ -1,9 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using Autofac;
+using EchoBotV3.State;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Dialogs.Internals;
+using Microsoft.Bot.Connector;
+using System.Configuration;
 using System.Web.Http;
-using System.Web.Routing;
 
 namespace EchoBotV3
 {
@@ -11,6 +12,16 @@ namespace EchoBotV3
     {
         protected void Application_Start()
         {
+            Conversation.UpdateContainer(builder =>
+            {
+                var store = new SqlServerBotDataStore(ConfigurationManager.ConnectionStrings["BotDataContextConnectionString"]
+                    .ConnectionString);
+                builder.Register(c => store)
+                    .As<IBotDataStore<BotData>>()
+                    .AsSelf()
+                    .SingleInstance();
+            });
+
             GlobalConfiguration.Configure(WebApiConfig.Register);
         }
     }
---
+++ EchoBotV3/State/SqlBotDataContextFactory.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.Entity.Infrastructure;
+
+namespace EchoBotV3.State
+{
+    public class SqlBotDataContextFactory : IDbContextFactory<SqlBotDataContext>
+    {
+        public SqlBotDataContext Create()
+        {
+            return new SqlBotDataContext("BotDataContextConnectionString");
+        }
+    }
+}
